f3deb6d1412ac20a3fc443b9720ceffa32d97f37
Shaurya Dev Aggarwal <shauryadev770@gmail.com>
2026-06-07 13:43:53 +0530

    Add files via upload

---
+++ Movie_Database_App.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import os
+
+while True:
+    movie_name = input("Search the movie name: ").lower()
+    url ="http://www.omdbapi.com/?"
+    params = {
+    "t" : movie_name,
+    "apikey" : "cd8f0024"
+    }
+    response = requests.get(url,params=params)
+    data = response.json()
+    
+    validation = data["Response"]
+    if validation == 'False' :
+        print("Invalid Name, Try again.")
+        continue
+    else:
+        break
+
+info = {
+    "Title" : data["Title"],
+    "Release date": data["Released"],
+    "Rated" : data["Rated"],
+    "Genre" : data["Genre"],
+    "Actors" : data["Actors"],
+    "Plot" : data["Plot"],
+    "Awards" : data["Awards"],
+    "IMDB Rating": data['Ratings'][0]['Value']
+}
+
+print(info)
+d = []
+if os.path.exists("Movie_Database.json") and os.path.getsize("Movie_Database.json") >0:
+    with open("Movie_Database.json","r") as file:
+       d =  json.load(file)
+
+d.append(info)
+
+with open("Movie_Database.json","w") as f:
+    json.dump(d,f,indent =4)
